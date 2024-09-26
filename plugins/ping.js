15d2e5fcd04a03c2086bc65537c483b557bbe920
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-09-26 19:52:08 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -2,6 +2,9 @@ const config = require('../config');
 const { cmd, commands } = require('../command');
 const { runtime } = require('../lib/functions');  
 
+// A helper function for sleep
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 cmd({
     pattern: "ping",
     desc: "To check ping",
@@ -9,10 +12,18 @@ cmd({
     filename: __filename,
 }, 
 async (Void, citel) => {
-    var inital = new Date().getTime();
+    var initial = new Date().getTime();
+
+    // Send initial ping testing message
     const { key } = await Void.sendMessage(citel.chat, { text: '```𝗧𝗲𝘀𝘁𝗶𝗻𝗴 𝗽𝗶𝗻𝗴 𝗼𝗳 𝗘𝗹𝗶𝘅𝗮...```' });
+
+    // Simulate delay
     var final = new Date().getTime();
-    await Pmd.sleep(1000);
-    const pg = await Void.sendMessage(citel.chat, { text: '📍 *ᴘɪɴɢ:' + (final - inital) + ' ms* ', edit: key });
+    await sleep(1000);
+
+    // Send the calculated ping
+    const pg = await Void.sendMessage(citel.chat, { text: '📍 *ᴘɪɴɢ: ' + (final - initial) + ' ms*' });
+
+    // Send reaction
     return await Void.sendMessage(citel.chat, { react: { text: '✔️', key: pg.key } });
 });
