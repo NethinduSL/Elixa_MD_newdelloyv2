0a53b51e130083ac83d103fa6eba02c7ec13dd0f
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-10-04 20:27:58 +0530

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -1,15 +1,15 @@
-const config = require('../config');
-const { cmd, commands } = require('../command');
-const maker = require('mumaker')
+//const config = require('../config');
+//const { cmd, commands } = require('../command');
+//const maker = require('mumaker')
 
 
 
-cmd({ pattern: "deepsea", alias: ["logo1"], category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text,q) => {
+//cmd({ pattern: "deepsea", alias: ["logo1"], category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text,q) => {
 
-        if (!text) return citel.reply('_Need text._')
+//        if (!text) return citel.reply('_Need text._')
 
-        let anu = await maker.textpro('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', q)
+//        let anu = await maker.textpro('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', q)
+//
+//        Void.sendMessage(citel.chat, { image: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 👨‍💻 ${tlang().greet}` }, { quoted: citel })
 
-        Void.sendMessage(citel.chat, { image: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 👨‍💻 ${tlang().greet}` }, { quoted: citel })
-
-    })
+//    })
