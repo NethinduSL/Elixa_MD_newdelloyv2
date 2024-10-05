73b0788f02fe1783fb12ca7824114395ce1b51d9
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-10-05 08:49:34 +0530

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -1,15 +1,45 @@
-//const config = require('../config');
-//const { cmd, commands } = require('../command');
-//const maker = require('mumaker')
+const config = require('../config');
+const { cmd, commands } = require('../command');
 
 
 
-//cmd({ pattern: "deepsea", alias: ["logo1"], category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text,q) => {
+cmd({
+        pattern: "repo",
+        alias: ["git", "sc", "script"],
+        desc: "Sends info about repo.",
+        category: "general",
+  react:"👍",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        let { data } = await axios.get('https://api.github.com/repos/Eboxsl/Elixa_MD')
+        let cap = `Hey ${citel.pushName}\n
+╭┈─────────────────────    .· * • ˚
+│*⭐ Total Stars:* ${data.stargazers_count} stars
+│*🍴 Forks:* ${data.forks_count} forks
+│*📡 Repo:* https://github.com/Eboxsl/Elixa_MD
+│*🏘Group:* https://github.com/Eboxsl/Elixa_MD
+│*🧑‍💻Deploy*:https://github.com/Eboxsl/Elixa_MD
+╰──────────────────────✬      * ˚  ✶`
+        let buttonMessaged = {
+            image: { url: await botpic() },
+            caption: cap,
+            footer: tlang().footer,
+            headerType: 4,
+            contextInfo: {
+                externalAdReply: {
+                    title: "IZUKU-Repo",
+                    body: "Easy to Use",
+                    thumbnail: log0,
+                    mediaType: 4,
+                    mediaUrl: '',
+                    sourceUrl: ``,
+                },
+            },
+        };
+        return await Void.sendMessage(citel.chat, buttonMessaged, {
+            quoted: citel,
+        });
 
-//        if (!text) return citel.reply('_Need text._')
-
-//        let anu = await maker.textpro('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', q)
-//
-//        Void.sendMessage(citel.chat, { image: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 👨‍💻 ${tlang().greet}` }, { quoted: citel })
-
-//    })
+    }
+)
